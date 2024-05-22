3e54e3bc3cc40623b2ea0d860e3c7fddaf03f590
Seanita Tolliver <seanitat@gmail.com>
2024-05-22 12:55:25 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import Services from './components/Services';
+import Tours from './components/Tours';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Tours />
+      <Services />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import { socialLinks } from '../data';
+import Pagelinks from './Pagelinks';
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <Pagelinks parentClass="footer-links" itemClass="footer-link" />
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                rel="noreferrer"
+                className="footer-icon"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
